3dbf88c4e2601639b61d19a0b5c795a81fb2d93d
siddiq <siddiq235@gmail.com>
2016-01-21 01:24:10 -0800

    updated sorting methods

--- InterviewPrep/src/Sorting/InsertionSort.java
+++ InterviewPrep/src/Sorting/InsertionSort.java
@@ -1,16 +1,22 @@
 package Sorting;
 
+import java.util.Arrays;
+
 public class InsertionSort {
+   public static void main(String[] args) {
+	int [] a={2,1,4,3};
+	System.out.println(Arrays.toString(sort(a)));
+}
 
 	public static int[] sort(int[] a) {
-      for(int i=1;i<a.length-1;i++){
+      for(int i=1;i<a.length;i++){
     	  int val=a[i];
     	  int temp=i;
     	  while(temp>0&&a[temp-1]>val){
     		  a[temp]=a[temp-1];
     		  temp--;
     	  }
-    	  a[temp]=val;
+    	  a[temp]=val; 
       }
 		return a;
 	}
--- InterviewPrep/src/Sorting/SortAlgosTest.java
+++ InterviewPrep/src/Sorting/SortAlgosTest.java
@@ -10,12 +10,12 @@ public class SortAlgosTest {
 
 	@Test
 	public void test() {
-	 int [] a={4,3,1,2,7,10,9,8,5,3,4};
-//	 SelectionSort.sort(a);
-//	 Util.printArray(SelectionSort.sort(a));
-//	 Util.printArray(BubbleSort.sort(a));
-//	 Util.printArray(InsertionSort.sort(a));
-//	 Util.printArray(MergeSort.sort(a));
+	 int [] a={4,3,1,2,7,10,9,8,-1,-4,-10,-7,5,3,4};
+
+	 Util.printArray(SelectionSort.sort(a));
+	 Util.printArray(BubbleSort.sort(a));
+	 Util.printArray(InsertionSort.sort(a));
+	 Util.printArray(MergeSort.sort(a));
 	 Util.printArray(QuickSort.sort(a));
 	}
 
