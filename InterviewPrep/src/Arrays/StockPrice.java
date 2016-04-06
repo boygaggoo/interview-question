21955514ada29c75d315b54573988f2d4b6af8c1
siddiq <siddiq235@gmail.com>
2016-04-05 18:22:23 -0700

    added new questions

--- InterviewPrep/src/Arrays/StockPrice.java
+++ InterviewPrep/src/Arrays/StockPrice.java
@@ -1,5 +1,8 @@
 package Arrays;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * The cost of a stock on each day is given in an array, 
  * find the max profit that you can make by buying and selling in those days. 
@@ -24,5 +27,109 @@ public class StockPrice {
 		}
 		return profit;
 	}
+/**
+ * If the array has size 0 or size 1, the maximum profit is 0.
+#    Otherwise:
+#       Split the array in half.
+#       Compute the maximum single-sell profit in the left array, call it L.
+#       Compute the maximum single-sell profit in the right array, call it R.
+#       Find the minimum of the first half of the array, call it Min
+#       Find the maximum of the second half of the array, call it Max
+#       Return the maximum of L, R, and Max - Min.
+# this algorithm uses O(n log n) time and O(log n) memory.
+
+ * @param a
+ * @return
+ */
+	public static int divideAndConquer(int[] a) {
+		if(a.length<=1)
+			return 0;
+		int []leftArray=Arrays.copyOfRange(a, 0, a.length/2);
+		int [] rightArray=Arrays.copyOfRange(a,  a.length/2,a.length);
+	    // Find the values for buying and selling purely in the left or purely in
+		// the right.
+		 int  leftBest  = divideAndConquer(leftArray);
+		  int rightBest = divideAndConquer(rightArray);
+		int min=findMin(leftArray);
+		int max=findMax(rightArray);
+		int crossBest=max-min;
+		return Math.max(crossBest,Math.max(leftBest, rightBest));
+	}
+private static int findMax(int[] rightArray) {
+	int max=Integer.MIN_VALUE;
+	for(int i:rightArray){
+		if(i>max){
+			max=i;
+		}
+	}
+	return max;
+}
+private static int findMin(int[] leftArray) {
+	int min=Integer.MAX_VALUE;
+	for(int i:leftArray){
+		if(i<min){
+			min=i;
+		}
+	}
+	return min;
+}
+/**
+ * # This gives the following simple and elegant algorithm for the maximum single-
+# sell profit problem:
+#
+#   Let profit = 0.
+#   Let min = arr[0]
+#   For k = 1 to length(arr):
+#       If arr[k] < min, set min = arr[k]
+#       If profit < arr[k] - min, set profit = arr[k] - min
+#
+# This is short, sweet, and uses only O(n) time and O(1) memory.  The beauty of
+# this solution is that we are quite naturally led there by thinking about how
+# to update our answer to the problem in response to seeing some new element.
+ * @param a
+ * @return
+ */
+public static int simpleSolution(int[] a) {
+	//[1,10,20,100]
+	if(a.length<=1)
+		return 0;
+	int min=a[0];//1
+	int profit=0;//0
+	for(int i=1;i<a.length;i++){//10,20,100
+		if(a[i]<min){//10,20,100
+			min=a[i];
+		}
+		if(profit<a[i]-min){//9,19,99
+			profit=a[i]-min;//9,19,99
+		}
+	}
+	return profit;
+}
+/**
+ * This is short, sweet, and uses only O(n) time and O(1) memory.  The beauty of
+# this solution is that we are quite naturally led there by thinking about how
+# to update our answer to the problem in response to seeing some new element.
+ * This method finds maxProft,index of sell and buy
+ * @param b
+ * @return
+ */
+public static int [] simpleSolutionWithIndexes(int[] a) {
+	int min=a[0],maxProfit=0;
+	int buyDate=0,sellDate=0;
+	int [] result={buyDate,sellDate,maxProfit};
+	for(int i=1;i<a.length;i++){
+		if(a[i]<min){
+			min=a[i];
+			buyDate=i;
+		}
+		int profit=a[i]-min;
+		if(maxProfit<profit){
+			maxProfit=profit;
+			sellDate=i;
+		}
+	}
+	result[0]=buyDate;result[1]=sellDate;result[2]=maxProfit;
+	return result;
+}
 
 }
--- InterviewPrep/src/Arrays/StockPriceTest.java
+++ InterviewPrep/src/Arrays/StockPriceTest.java
@@ -2,6 +2,8 @@ package Arrays;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 public class StockPriceTest {
@@ -11,7 +13,24 @@ public class StockPriceTest {
 		int [] a={1,4,5,11};
 		int [] b={10,11,1,12};
 		assertEquals(10,StockPrice.bruteForce(a));
-		assertEquals(11,StockPrice.bruteForce(a));
+		assertEquals(11,StockPrice.bruteForce(b));
+		
+	}
+	@Test
+	public void divideAndConquerTest() {
+		int [] a={1,4,5,11};
+		int [] b={10,11,1,12};
+		assertEquals(10,StockPrice.divideAndConquer(a));
+		assertEquals(11,StockPrice.bruteForce(b));
+		
+	}
+	@Test
+	public void simpleSolTest() {
+		int [] a={1,4,5,11};
+		int [] b={10,11,1,12};
+		assertEquals(10,StockPrice.simpleSolution(a));
+		assertEquals(11,StockPrice.simpleSolution(b));
+		assertEquals(11,StockPrice.simpleSolutionWithIndexes(b)[2]);
 		
 	}
 
