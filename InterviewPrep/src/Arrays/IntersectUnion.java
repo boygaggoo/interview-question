4bb8d9507d80f9be33ee6faa9c48a6e0a7cd3945
siddiq <siddiq235@gmail.com>
2016-05-15 20:49:43 -0700

    modifed

--- InterviewPrep/src/Arrays/IntersectUnion.java
+++ InterviewPrep/src/Arrays/IntersectUnion.java
@@ -4,7 +4,7 @@ public class IntersectUnion {
 	public static void main(String[] args) {
 		int [] a={1,2,3,4};
 		int [] b={3,4,5,6,7};
-//		union(a,b);
+		union(a,b);
 		intersect(a,b);
 	}
 
--- InterviewPrep/src/Arrays/MissingNumber.java
+++ InterviewPrep/src/Arrays/MissingNumber.java
@@ -2,8 +2,8 @@ package Arrays;
 
 public class MissingNumber {
 public static void main(String[] args) {
-	int []a={1,2};
-	findMissingNumber(a,3);
+	int []a={1,2,3,5};
+	findMissingNumber(a,5);
 }
 
 private static void findMissingNumber(int[] a,int n) {
