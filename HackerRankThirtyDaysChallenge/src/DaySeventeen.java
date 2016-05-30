bd729fd85d6de94dac7c40689bc96c8bc1237c13
siddiq <siddiq235@gmail.com>
2016-05-30 00:05:53 -0700

    solved exception handling

---
+++ HackerRankThirtyDaysChallenge/src/DaySeventeen.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class DaySeventeen {
+    public static void main(String[] args) {
+	        Scanner in = new Scanner(System.in);
+	        String S = in.next();
+	        try{
+	        	int num=Integer.parseInt(S);
+	        	System.out.println(num);
+	        }catch(Exception e){
+	        	System.out.println("Bad String");
+	        }
+	    }
+	
+
+}
