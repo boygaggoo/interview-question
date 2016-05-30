5063d3d68401cd5eb63509ae52de837d28e7be37
siddiq <siddiq235@gmail.com>
2016-05-30 15:18:10 -0700

    stack and queue

---
+++ HackerRankThirtyDaysChallenge/src/DayNineteen.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class DayNineteen {
+	
+	 Stack<Character> stk=new Stack<Character>();
+	 Queue<Character> que=new LinkedList<Character>();
+	
+	
+	
+	 public static void main(String[] args) {
+	        Scanner scan = new Scanner(System.in);
+	        String input = scan.nextLine();
+	        scan.close();
+
+	        // Convert input String to an array of characters:
+	        char[] s = input.toCharArray();
+
+	        // Create a Solution object:
+	        DayNineteen p = new DayNineteen();
+
+	        // Enqueue/Push all chars to their respective data structures:
+	        for (char c : s) {
+	            p.pushCharacter(c);
+	            p.enqueueCharacter(c);
+	        }
+
+	        // Pop/Dequeue the chars at the head of both data structures and compare them:
+	        boolean isPalindrome = true;
+	        for (int i = 0; i < s.length/2; i++) {
+	            if (p.popCharacter() != p.dequeueCharacter()) {
+	                isPalindrome = false;                
+	                break;
+	            }
+	        }
+
+	        //Finally, print whether string s is palindrome or not.
+	        System.out.println( "The word, " + input + ", is " 
+	                           + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+	    }
+
+	private Character dequeueCharacter() {
+		// TODO Auto-generated method stub
+		return que.remove();
+	}
+
+	private Character popCharacter() {
+		// TODO Auto-generated method stub
+		return stk.pop();
+	}
+
+	private void enqueueCharacter(char c) {
+		que.add(c);
+		
+	}
+
+	private void pushCharacter(char c) {
+		stk.add(c);
+		
+	}
+	
+}
